276be26596c849f1fe0d4d78c55f50add6460482
Serge Huber <shuber@jahia.com>
2024-01-11 07:52:12 +0100

    BACKLOG-22085 Initial JSDoc tests.

--- utils/getChildNodes.js
+++ utils/getChildNodes.js
@@ -1,3 +1,10 @@
+/**
+ * Returns an array of child nodes of a given node.
+ * @param {JCRNodeWrapper} node - The node to get the child nodes from.
+ * @param {number} limit - The maximum number of nodes to return (defaults to 100).
+ * @param {function} filter - A function to filter the nodes to be returned.
+ * @returns {JCRNodeWrapper[]} An array of child nodes.
+ */
 export default (node, limit = 100, filter = undefined) => {
     let result = [];
     if (node) {
--- utils/getNodeFromPathOrId.js
+++ utils/getNodeFromPathOrId.js
@@ -1,3 +1,11 @@
+/**
+ * Returns a node from a path or identifier
+ * @param {object} props - The properties object containing either the node identifier or the node path.
+ * @param {string} props.identifier - The node identifier.
+ * @param {string} props.path - The node path.
+ * @param {JCRSessionWrapper} session - The JCR session.
+ * @returns {JCRNodeWrapper} The node.
+ */
 export default (props, session) => {
     if (props.identifier) {
         return session.getNodeByIdentifier(props.identifier);
