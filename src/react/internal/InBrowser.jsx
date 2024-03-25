03d9f6c9e007ecec65f81efe68bb2e84628aab58
kevan Jahanshahi <kevan@jahia.com>
2024-03-25 15:48:20 +0100

    BACKLOG-22520: introduce window.__APPSHELL_INIT_DATA__ for client side hydration/rendering (#26)
    * BACKLOG-22520: introduce window.__APPSHELL_INIT_DATA__ for client side hydration/rendering
    
    * BACKLOG-22520: fix linter

--- src/react/internal/InBrowser.jsx
+++ src/react/internal/InBrowser.jsx
@@ -15,22 +15,35 @@ const getClientI18nStoreScript = (i18nStore, namespace, lang) => {
         filteredI18nStore[lang][namespace] = i18nStore[lang][namespace];
 
         return '<script type="text/javascript">\n' +
-            '        if(!window.__INITIAL_I18N_STORES__) {\n' +
-            '            window.__INITIAL_I18N_STORES__ = [];\n' +
+            '        if(!window.__APPSHELL_INIT_DATA__) {\n' +
+            '            window.__APPSHELL_INIT_DATA__ = {};\n' +
             '        }\n' +
-            `        window.__INITIAL_I18N_STORES__.push(${JSON.stringify(filteredI18nStore)});\n` +
+            '        if(!window.__APPSHELL_INIT_DATA__.initialI18nStore) {\n' +
+            '            window.__APPSHELL_INIT_DATA__.initialI18nStore = [];\n' +
+            '        }\n' +
+            `        window.__APPSHELL_INIT_DATA__.initialI18nStore.push(${JSON.stringify(filteredI18nStore)});\n` +
             '    </script>';
     }
+};
+
+const getAppShellInitData = moduleBaseUrl => {
+    return '<script type="text/javascript">\n' +
+        '        if(!window.__APPSHELL_INIT_DATA__) {\n' +
+        '            window.__APPSHELL_INIT_DATA__ = {};\n' +
+        '        }\n' +
+        `        window.__APPSHELL_INIT_DATA__.moduleBaseUrl = '${moduleBaseUrl}';\n` +
+        '    </script>';
 };
 
 function InBrowser({child: Child, props, dataKey}) {
     const {bundleKey, currentResource, renderContext} = useServerContext();
     const {initialI18nStore, initialLanguage} = getInitialProps();
 
+    const appShellInitDataScript = getAppShellInitData(buildUrl({value: '/modules'}, renderContext, currentResource));
+    const i18nScript = getClientI18nStoreScript(initialI18nStore, bundleKey, initialLanguage);
     // The paths are absolute here to avoid jAddResources to look for .js in other modules
     const remote = buildUrl({value: '/modules/' + bundleKey + '/javascript/client/remote.js'}, renderContext, currentResource);
     const appShell = buildUrl({value: '/modules/npm-modules-engine/javascript/apps/reactAppShell.js'}, renderContext, currentResource);
-    const i18nScript = getClientI18nStoreScript(initialI18nStore, bundleKey, initialLanguage);
 
     const data = {};
     data[dataKey] = encodeURIComponent(JSON.stringify({
@@ -49,6 +62,7 @@ function InBrowser({child: Child, props, dataKey}) {
             </div>
 
             {i18nScript && <AddResources key={`i18n_initialStore_${bundleKey}`} insert inlineResource={i18nScript}/>}
+            <AddResources key="npm-engine-appShellInitData" insert inlineResource={appShellInitDataScript}/>
             <AddResources insert type="javascript" targetTag="body" resources={remote}/>
             <AddResources type="javascript" targetTag="body" resources={appShell}/>
         </>
