e9d6d85f37a4a589e0757ed42b53c637d920b3be
Tim ROBERT <robert.tim.web@gmail.com>
2024-07-17 16:29:35 +0200

    Use JSDoc doc to generate the JahiaComponent type and remove the manual type

--- src/react/defineJahiaComponent.jsx
+++ src/react/defineJahiaComponent.jsx
@@ -1,9 +1,17 @@
-import {JahiaComponent} from '../../types/manual/JahiaComponent';
 /**
- * Using this function provides autocomplete on the jahiaComponent properties
- * @param {object} jahiaComponent an object containing the jahia component to define
- * @returns {object} JahiaComponent
+ * @typedef {Object} JahiaComponent
+ * @property {string} name - The name of the component.
+ * @property {string} displayName - The display name of the component (in jahia's UI).
+ * @property {"template"|"view"} componentType - The type of the component.
+ * @property {string} nodeType - The content node type the component applies to.
+ * @property {Record<string, string>} properties - Properties to add on the component
  */
-export function defineJahiaComponent(jahiaComponent : JahiaComponent) {
+
+/**
+ * Using this function provides autocomplete on the jahiaComponent properties.
+ * @param {JahiaComponent} jahiaComponent - An object containing the Jahia component to define.
+ * @returns {JahiaComponent} The jahiaComponent object.
+ */
+export function defineJahiaComponent(jahiaComponent) {
     return jahiaComponent;
 }
