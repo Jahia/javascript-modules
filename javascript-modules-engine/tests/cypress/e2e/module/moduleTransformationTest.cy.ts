3b75c2f99061ad05f1ec85409fff8210fd5783d0
Baptiste Grimaud <bgrimaud@jahia.com>
2025-02-19 18:14:22 +0100

    fix: Remove test on Jahia-Depends

--- javascript-modules-engine/tests/cypress/e2e/module/moduleTransformationTest.cy.ts
+++ javascript-modules-engine/tests/cypress/e2e/module/moduleTransformationTest.cy.ts
@@ -13,7 +13,8 @@ describe('Check that the Javascript module has been transformed properly and has
             expect(result).to.contain('Bundle-SymbolicName: jahia-javascript-module-example')
             expect(result).to.contain('Bundle-Vendor: Jahia Solutions Group SA')
             expect(result).to.contain('Bundle-Version: 1.0.0')
-            expect(result).to.contain('Jahia-Depends: default,legacy-default-components,javascript-modules-engine')
+            // TODO to enable once javascript-modules-engine >= 0.4.0 is included in jahia-pack
+            // expect(result).to.contain('Jahia-Depends: default,legacy-default-components,javascript-modules-engine')
             expect(result).to.contain('Jahia-Module-Type: templatesSet')
             expect(result).to.contain('Jahia-javascript-InitScript: dist/main.js')
             expect(result).to.contain('Jahia-Required-Version: 8.2.0.0-SNAPSHOT')
