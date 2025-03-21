d424f3cfc12f407eb129eb59ee6a61b076c8b690
Baptiste Grimaud <bgrimaud@jahia.com>
2025-03-21 11:14:03 +0100

    feat!: Refactor <Area> and <ContentArea> components

--- jahia-test-module/src/react/server/views/testAbsoluteAreas/TestAbsoluteAreas.tsx
+++ jahia-test-module/src/react/server/views/testAbsoluteAreas/TestAbsoluteAreas.tsx
@@ -7,79 +7,81 @@ jahiaComponent(
     displayName: "Test Absolute Areas (react)",
     componentType: "view",
   },
-  () => (
+  (_, { currentNode, renderContext }) => (
     <>
       <h2>React JAbsoluteArea test component</h2>
 
       <h2>Basic Area</h2>
       <div data-testid="basicArea">
-        <AbsoluteArea name={"basicArea"} level={1} />
+        <AbsoluteArea name={"basicArea"} parent={currentNode} />
       </div>
 
       <h2>Area with allowed types</h2>
       <div data-testid="allowedTypesArea">
         <AbsoluteArea
           name={"allowedTypesArea"}
-          allowedTypes={["jnt:event", "jnt:bigText"]}
-          level={1}
+          parent={currentNode}
+          allowedNodeTypes={["jnt:event", "jnt:bigText"]}
         />
       </div>
 
       <h2>Area with number of items</h2>
       <div data-testid="numberOfItemsArea">
-        <AbsoluteArea name={"numberOfItemsArea"} numberOfItems={2} level={1} />
+        <AbsoluteArea name={"numberOfItemsArea"} parent={currentNode} numberOfItems={2} />
       </div>
 
       <h2>Area with areaView</h2>
       <div data-testid="areaViewArea">
-        <AbsoluteArea name={"areaViewArea"} areaView={"dropdown"} level={1} />
+        <AbsoluteArea name={"areaViewArea"} parent={currentNode} view={"dropdown"} />
       </div>
 
-      <h2>Area with subNodesView</h2>
-      <div data-testid="subNodesViewArea">
-        <AbsoluteArea name={"subNodesViewArea"} subNodesView={"link"} level={1} />
+      <h2>Area with parent</h2>
+      <div data-testid="parentArea">
+        <AbsoluteArea name="subLevel" parent={currentNode.getNode("basicArea")} />
       </div>
 
-      <h2>Area with path</h2>
-      <div data-testid="pathArea">
-        <AbsoluteArea path={"basicArea/subLevel"} level={1} />
+      <h2>Absolute Area with home page</h2>
+      <div data-testid="absoluteAreaHomePage">
+        <AbsoluteArea name="pagecontent" parent={renderContext.getSite().getHome()} />
       </div>
 
-      <h2>Absolute Area with home page content</h2>
-      <div data-testid="absoluteArea">
-        <AbsoluteArea name="pagecontent" />
+      <h2>Absolute Area with custom page (sub-level)</h2>
+      <div data-testid="absoluteAreaCustomPage">
+        <AbsoluteArea
+          name="pagecontent"
+          parent={renderContext.getSite().getNode("custom/sub-level")}
+        />
       </div>
 
       <h2>Non editable area </h2>
       <div data-testid="nonEditableArea">
-        <AbsoluteArea name="nonEditable" readOnly level={1} />
-      </div>
-
-      <h2>Absolute area level </h2>
-      <div data-testid="absoluteAreaLevel">
-        <AbsoluteArea name="pagecontent" level={0} />
+        <AbsoluteArea name="nonEditable" parent={currentNode} readOnly />
       </div>
 
       <h2>Area type</h2>
       <div data-testid="areaType">
-        <AbsoluteArea name="areaType" areaType="javascriptExample:testAreaColumns" level={1} />
+        <AbsoluteArea
+          name="areaType"
+          parent={currentNode}
+          nodeType="javascriptExample:testAreaColumns"
+        />
       </div>
 
       <h2>Limited absolute area editing</h2>
       <div data-testid="limitedAbsoluteAreaEdit">
-        <AbsoluteArea name="pagecontent" readOnly="children" />
+        <AbsoluteArea name="pagecontent" parent={currentNode} readOnly="children" />
       </div>
 
       <h2>Absolute Area parameters</h2>
       <div data-testid="absoluteAreaParameters">
         <AbsoluteArea
           name="absoluteAreaParameters"
-          areaView="parameters"
+          parent={currentNode}
+          view="parameters"
           parameters={{
             stringParam1: "stringValue1",
             stringParam2: "stringValue2",
           }}
-          level={1}
         />
       </div>
     </>
