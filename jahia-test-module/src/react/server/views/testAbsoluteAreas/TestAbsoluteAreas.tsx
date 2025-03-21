e22940d3e4309b2b82b676791119c2d63879d50d
Baptiste Grimaud <bgrimaud@jahia.com>
2025-03-21 11:14:03 +0100

    feat!: Refactor <Area> and <ContentArea> components

--- jahia-test-module/src/react/server/views/testAbsoluteAreas/TestAbsoluteAreas.tsx
+++ jahia-test-module/src/react/server/views/testAbsoluteAreas/TestAbsoluteAreas.tsx
@@ -7,42 +7,38 @@ jahiaComponent(
     displayName: "Test Absolute Areas (react)",
     componentType: "view",
   },
-  () => (
+  (_, { currentNode }) => (
     <>
       <h2>React JAbsoluteArea test component</h2>
 
       <h2>Basic Area</h2>
       <div data-testid="basicArea">
-        <AbsoluteArea name={"basicArea"} level={1} />
+        <AbsoluteArea name={"basicArea"} />
       </div>
 
       <h2>Area with allowed types</h2>
       <div data-testid="allowedTypesArea">
-        <AbsoluteArea
-          name={"allowedTypesArea"}
-          allowedTypes={["jnt:event", "jnt:bigText"]}
-          level={1}
-        />
+        <AbsoluteArea name={"allowedTypesArea"} allowedNodeTypes={["jnt:event", "jnt:bigText"]} />
       </div>
 
       <h2>Area with number of items</h2>
       <div data-testid="numberOfItemsArea">
-        <AbsoluteArea name={"numberOfItemsArea"} numberOfItems={2} level={1} />
+        <AbsoluteArea name={"numberOfItemsArea"} numberOfItems={2} />
       </div>
 
       <h2>Area with areaView</h2>
       <div data-testid="areaViewArea">
-        <AbsoluteArea name={"areaViewArea"} areaView={"dropdown"} level={1} />
+        <AbsoluteArea name={"areaViewArea"} view={"dropdown"} />
       </div>
 
       <h2>Area with subNodesView</h2>
       <div data-testid="subNodesViewArea">
-        <AbsoluteArea name={"subNodesViewArea"} subNodesView={"link"} level={1} />
+        <AbsoluteArea name={"subNodesViewArea"} />
       </div>
 
       <h2>Area with path</h2>
       <div data-testid="pathArea">
-        <AbsoluteArea path={"basicArea/subLevel"} level={1} />
+        <AbsoluteArea name={"subLevel"} parent={currentNode.getNode("basicArea")} />
       </div>
 
       <h2>Absolute Area with home page content</h2>
@@ -52,17 +48,17 @@ jahiaComponent(
 
       <h2>Non editable area </h2>
       <div data-testid="nonEditableArea">
-        <AbsoluteArea name="nonEditable" readOnly level={1} />
+        <AbsoluteArea name="nonEditable" readOnly />
       </div>
 
       <h2>Absolute area level </h2>
       <div data-testid="absoluteAreaLevel">
-        <AbsoluteArea name="pagecontent" level={0} />
+        <AbsoluteArea name="pagecontent" />
       </div>
 
       <h2>Area type</h2>
       <div data-testid="areaType">
-        <AbsoluteArea name="areaType" areaType="javascriptExample:testAreaColumns" level={1} />
+        <AbsoluteArea name="areaType" nodeType="javascriptExample:testAreaColumns" />
       </div>
 
       <h2>Limited absolute area editing</h2>
@@ -74,12 +70,11 @@ jahiaComponent(
       <div data-testid="absoluteAreaParameters">
         <AbsoluteArea
           name="absoluteAreaParameters"
-          areaView="parameters"
+          view="parameters"
           parameters={{
             stringParam1: "stringValue1",
             stringParam2: "stringValue2",
           }}
-          level={1}
         />
       </div>
     </>
