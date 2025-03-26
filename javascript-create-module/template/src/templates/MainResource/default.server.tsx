81e21b008b0699919a81b8ba1cffdcaf197e6eea
Baptiste Grimaud <bgrimaud@jahia.com>
2025-03-26 09:57:32 +0100

    set priority for main resource of HelloWorld

--- javascript-create-module/template/src/templates/MainResource/default.server.tsx
+++ javascript-create-module/template/src/templates/MainResource/default.server.tsx
@@ -5,6 +5,7 @@ jahiaComponent(
   {
     componentType: "template",
     nodeType: "jmix:mainResource",
+    priority: -1, // allow to overwrite this template by defining a component with a higher priority. When not specified, the default priority is 0
   },
   ({ "jcr:title": title }, { currentNode }) => (
     <Layout title={title}>
