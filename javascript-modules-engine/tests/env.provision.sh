e77807fb3cf3f4ae315a91ddb6064f1f519c2bc1
Baptiste Grimaud <bgrimaud@jahia.com>
2025-04-01 10:06:33 +0200

    copy env.run.sh

--- javascript-modules-engine/tests/env.provision.sh
+++ javascript-modules-engine/tests/env.provision.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # This is a copy of https://github.com/Jahia/jahia-cypress/blob/main/env.provision.sh
-# with an extra step to install the prepackaged site AFTER the artifacts are installed.
-# See custom code between OVERWRITE-BEGIN and OVERWRITE-END comments
 
 # This script can be used to warmup the environment and execute the tests
 # It is used by the docker image at startup
@@ -107,14 +105,3 @@ if [[ -d scripts/ ]]; then
   cd ..
 fi
 
-# OVERWRITE-BEGIN
-POST_PROVISIONING_MANIFEST="post-provisioning-manifest.yml"
-echo "$(date +'%d %B %Y - %k:%M') == Executing post-provisioning manifest: ${POST_PROVISIONING_MANIFEST} =="
-curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script="@./${POST_PROVISIONING_MANIFEST};type=text/yaml" $(find assets -type f | sed -E 's/^(.+)$/--form file=\"@\1\"/' | xargs)
-echo
-if [[ $? -eq 1 ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') == POST PROVISIONING FAILURE - EXITING SCRIPT, NOT RUNNING THE TESTS"
-  echo "failure" > ./results/test_failure
-  exit 1
-fi
-# OVERWRITE-END
--- javascript-modules-engine/tests/env.run.sh
+++ javascript-modules-engine/tests/env.run.sh
@@ -1,4 +1,84 @@
 #!/bin/bash
-version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
-echo Using @jahia/cypress@$version...
-npx --yes --package @jahia/cypress@$version env.run
+# This is a copy of https://github.com/Jahia/jahia-cypress/blob/main/env.run.sh.
+# The only difference is an extra step right after the usual provisioning (with env.provisioning.sh) to install the prepackaged site AFTER the artifacts are installed.
+# See env.post-provision.sh added between OVERWRITE-BEGIN and OVERWRITE-END
+
+# This script can be used to warmup the environment and execute the tests
+# It is used by the docker image at startup
+
+BASEDIR=$(dirname $(readlink -f $0))
+
+bash $BASEDIR/env.provision.sh
+
+# OVERWRITE-BEGIN
+bash $BASEDIR/env.post-provision.sh
+# OVERWRITE-END
+
+source $BASEDIR/set-env.sh
+
+MODE=${MODE:-ci}
+
+echo "$(date +'%d %B %Y - %k:%M') == env.run.sh == Printing the most important environment variables"
+echo "$(date +'%d %B %Y - %k:%M') == NEXUS_USERNAME: ${NEXUS_USERNAME:0:3}***${NEXUS_USERNAME:(-6)}"
+echo "$(date +'%d %B %Y - %k:%M') == DEBUG: ${DEBUG}"
+echo "$(date +'%d %B %Y - %k:%M') == CYPRESS MODE: ${MODE}"
+
+if [[ "${DEBUG}" == "true" ]]; then
+  touch /tmp/debug
+fi
+
+echo "$(date +'%d %B %Y - %k:%M') == Fetching the list of installed modules =="
+bash -c "unset npm_config_package; npx --yes @jahia/jahia-reporter@latest utils:modules \
+  --moduleId=\"${MODULE_ID}\" \
+  --jahiaUrl=\"${JAHIA_URL}\" \
+  --jahiaPassword=\"${SUPER_USER_PASSWORD}\" \
+  --filepath=\"results/installed-jahia-modules.json\""
+echo "$(date +'%d %B %Y - %k:%M') == Modules fetched =="
+INSTALLED_MODULE_VERSION=$(cat results/installed-jahia-modules.json | jq '.module.version')
+if [[ $INSTALLED_MODULE_VERSION == "UNKNOWN" ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') ERROR: Unable to detect module: ${MODULE_ID} on the remote system "
+  echo "$(date +'%d %B %Y - %k:%M') ERROR: The Script will exit"
+  echo "$(date +'%d %B %Y - %k:%M') ERROR: Tests will NOT run"
+  echo "failure" > ./results/test_failure
+  exit 1
+fi
+
+echo "$(date +'%d %B %Y - %k:%M') == Run tests =="
+mkdir -p ./results/reports
+rm -rf ./results/reports
+
+if [[ -z "${CYPRESS_CONFIGURATION_FILE}" ]]; then
+  CYPRESS_CONFIGURATION_FILE=cypress.config.ts
+fi
+
+if [[ "${TESTS_PROFILE}" != "" ]]; then
+  CYPRESS_CONFIGURATION_FILE=${TESTS_PROFILE}
+else
+  CYPRESS_CONFIGURATION_FILE="cypress.config.ts"
+fi
+
+echo "$(date +'%d %B %Y - %k:%M') == Running Cypress with configuration file ${CYPRESS_CONFIGURATION_FILE} =="
+
+NO_COLOR=1 yarn e2e:${MODE} --config-file "${CYPRESS_CONFIGURATION_FILE}"
+
+if [[ $? -eq 0 ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') == Full execution successful =="
+  echo "success" > ./results/test_success
+  yarn report:merge; yarn report:html
+else
+  echo "$(date +'%d %B %Y - %k:%M') == One or more failed tests =="
+  echo "failure" > ./results/test_failure
+  yarn report:merge; yarn report:html
+fi
+
+WAIT_DURATION=0
+while [[ -e /tmp/debug ]]; do
+  echo "Debug file present - $(( ++ WAIT_DURATION ))s - waiting for file removal or expiration of GitHub Actions timeout..."
+  sleep 1;
+done
+
+if [[ -e ./results/test_success ]]; then
+  exit 0
+else
+  exit 1
+fi
