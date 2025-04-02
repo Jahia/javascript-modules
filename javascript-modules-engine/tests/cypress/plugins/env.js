933a1e641d19cb4366806249622c8221d1b7dce1
Baptiste Grimaud <bgrimaud@jahia.com>
2025-04-02 10:57:47 +0200

    remove unnecessary env variable injection

--- javascript-modules-engine/tests/cypress/plugins/env.js
+++ javascript-modules-engine/tests/cypress/plugins/env.js
@@ -1,7 +1,6 @@
 module.exports = (on, config) => {
     config.baseUrl = process.env.JAHIA_URL
     config.env.SUPER_USER_PASSWORD = process.env.SUPER_USER_PASSWORD
-    config.env.PREPACKAGED_SITE_URL = process.env.CYPRESS_PREPACKAGED_SITE_URL
 
     return config
 }
