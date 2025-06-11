2322ebcaebf9e31c4fb1d16eff460ed9d95603fb
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-06-11 09:07:45 +0200

    indeed

--- javascript-create-module/template/vite.config.mjs
+++ javascript-create-module/template/vite.config.mjs
@@ -5,6 +5,19 @@ import { spawnSync } from "node:child_process";
 export default defineConfig({
   plugins: [
     jahia({
+      // Default values:
+      // inputDir: "src",
+      // outputDir: "dist",
+      // assetsDir: "assets",
+      // client: {
+      //   inputGlob: "**/*.client.{jsx,tsx}",
+      //   outputDir: "client",
+      // },
+      // server: {
+      //   inputGlob: "**/*.server.{ts,tsx}",
+      //   outputFile: "server/index.js",
+      // },
+
       // This function is called every time a build succeeds in watch mode
       watchCallback() {
         spawnSync("yarn", ["watch:callback"], { stdio: "inherit", shell: true });
