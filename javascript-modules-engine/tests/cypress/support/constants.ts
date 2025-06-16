e0f0c386b82c528a5c0f54c9b643bc15f34187ea
Volodymyr Tereshchuk <vtereshchuk@jahia.com>
2025-06-16 16:35:25 +0200

    chore: use single quotes for all constants

--- javascript-modules-engine/tests/cypress/support/constants.ts
+++ javascript-modules-engine/tests/cypress/support/constants.ts
@@ -1,19 +1,19 @@
 export const GENERIC_SITE_KEY = 'javascriptTestSite';
-export const HYDROGEN_SITE_KEY = "hydrogen";
-export const HYDROGEN_PREPACKAGED_SITE="jar:mvn:org.jahia.samples/javascript-modules-samples-hydrogen-prepackaged/LATEST/zip!/site.zip";
+export const HYDROGEN_SITE_KEY = 'hydrogen';
+export const HYDROGEN_PREPACKAGED_SITE='jar:mvn:org.jahia.samples/javascript-modules-samples-hydrogen-prepackaged/LATEST/zip!/site.zip';
 export const HYDROGEN_POSTS = [
   {
-    page: "a-second-article",
-    title: "A second Article",
-    subTitle: "We love writing articles!",
-    details: "Written by Alice, Bob on March 28, 2025",
-    extract: "This is a great article",
+    page: 'a-second-article',
+    title: 'A second Article',
+    subTitle: 'We love writing articles!',
+    details: 'Written by Alice, Bob on March 28, 2025',
+    extract: 'This is a great article',
   },
   {
-    page: "welcome-to-hydrogen-blog",
-    title: "Welcome to Hydrogen Blog",
-    subTitle: "This blog will keep you up to date with the latest news from our company",
-    details: "Written by Gautier on March 26, 2025",
+    page: 'welcome-to-hydrogen-blog',
+    title: 'Welcome to Hydrogen Blog',
+    subTitle: 'This blog will keep you up to date with the latest news from our company',
+    details: 'Written by Gautier on March 26, 2025',
     extract:
       '<b>Hydrogen</b>&nbsp;is a&nbsp;<a href="https://en.wikipedia.org/wiki/Chemical_element" title="Chemical element">chemical element</a>;',
   },
