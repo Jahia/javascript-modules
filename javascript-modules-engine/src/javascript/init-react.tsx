ac595f90f6187234111065bab1a2364c3982b5e0
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-08-29 08:56:31 +0200

    remove styled-jsx

--- javascript-modules-engine/src/javascript/init-react.tsx
+++ javascript-modules-engine/src/javascript/init-react.tsx
@@ -5,7 +5,6 @@ import type { Bundle } from "org.osgi.framework";
 import type { ComponentType } from "react";
 import ReactDOMServer from "react-dom/server.edge";
 import { I18nextProvider } from "react-i18next";
-import { createStyleRegistry, StyleRegistry } from "styled-jsx";
 
 server.registry.add("view", "react", {
   viewRenderer: "react",
@@ -28,44 +27,38 @@ server.registry.add("viewRenderer", "react", {
     i18n.changeLanguage(language);
 
     // SSR
-    const styleRegistry = createStyleRegistry();
     const currentNode = currentResource.getNode();
     const mainNode = renderContext.getMainResource().getNode();
     const View = view.component;
     const element = (
-      <StyleRegistry registry={styleRegistry}>
-        <ServerContextProvider
-          renderContext={renderContext}
-          currentResource={currentResource}
-          currentNode={currentNode}
-          mainNode={mainNode}
-          jcrSession={currentNode.getSession()}
-          bundleKey={bundleKey}
-        >
-          <I18nextProvider i18n={i18n} />
+      <ServerContextProvider
+        renderContext={renderContext}
+        currentResource={currentResource}
+        currentNode={currentNode}
+        mainNode={mainNode}
+        jcrSession={currentNode.getSession()}
+        bundleKey={bundleKey}
+      >
+        <I18nextProvider i18n={i18n}>
           <View />
-        </ServerContextProvider>
-      </StyleRegistry>
+        </I18nextProvider>
+      </ServerContextProvider>
     );
 
-    const html = ReactDOMServer.renderToString(element)
+    const html =
+      // In page mode, prepend the rendered HTML with the HTML5 doctype
+      (currentResource.getContextConfiguration() === "page" ? "<!DOCTYPE html>" : "") +
       // We use a `<jsm-raw-html>` element to wrap raw HTML output because React does not allow
       // directly returning raw HTML strings. These elements are removed there, to avoid
       // having them in the final output.
       // `<jsm-raw-html>` SHOULD NOT be used in userland code, it is an internal implementation
       // detail.
-      .replaceAll(/<\/?jsm-raw-html>/g, "");
-
-    const styles = ReactDOMServer.renderToStaticMarkup(styleRegistry.styles());
-    const stylesResource = styles
-      ? `<jahia:resource type="inline" key="styles${currentNode.getIdentifier()}">${styles}</jahia:resource>`
-      : "";
+      ReactDOMServer.renderToString(element).replaceAll(/<\/?jsm-raw-html>/g, "");
 
     if (currentResource.getContextConfiguration() === "page") {
-      // Set the HTML5 doctype that can't be rendered in JSX
-      return `<!DOCTYPE html>${html}${stylesResource}`;
+      return `<!DOCTYPE html>${html}`;
     }
 
-    return `${html}${stylesResource}`;
+    return html;
   },
 });
