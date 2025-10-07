deec8a1db4ecca84d304dfe2d47674916971b381
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-10-07 10:25:58 +0200

    feat: expose react-dom

---
+++ javascript-modules-engine/src/shared/react-dom.ts
@@ -0,0 +1,14 @@
+export {
+  createPortal,
+  flushSync,
+  preconnect,
+  prefetchDNS,
+  preinit,
+  preinitModule,
+  preload,
+  preloadModule,
+  requestFormReset,
+  unstable_batchedUpdates,
+  useFormState,
+  useFormStatus,
+} from "react-dom";
