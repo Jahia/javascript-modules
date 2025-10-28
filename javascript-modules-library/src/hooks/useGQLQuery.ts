9def1b00fe4e918a727158e1cd7b6d1a93fa7274
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-10-28 11:48:42 +0100

    feat(library): support typed graphql document

--- javascript-modules-library/src/hooks/useGQLQuery.ts
+++ javascript-modules-library/src/hooks/useGQLQuery.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { print } from "@0no-co/graphql.web";
+import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
 import type { GraphQLFormattedError } from "graphql";
 import { useServerContext } from "./useServerContext.js";
 
@@ -6,27 +9,43 @@ import { useServerContext } from "./useServerContext.js";
  *
  * @returns The result of the query
  */
-export const useGQLQuery = ({
+export function useGQLQuery<TData = any, TVariables = Record<string, any>>({
+  query,
+  variables,
+  operationName,
+}: {
+  query: TypedDocumentNode<TData, TVariables>;
+  variables?: TVariables;
+  operationName?: string;
+}): { data: TData; errors?: GraphQLFormattedError[] };
+export function useGQLQuery({
+  query,
+  variables,
+  operationName,
+}: {
+  query: string;
+  variables?: Record<string, unknown>;
+  operationName?: string;
+}): { data: any; errors?: GraphQLFormattedError[] };
+export function useGQLQuery({
   query,
   variables,
   operationName,
 }: {
   /** The GraphQL query to execute */
-  query: string;
+  query: string | TypedDocumentNode;
   /** The variables to use for the query */
   variables?: Record<string, unknown>;
   /** The operation name to use for the query */
   operationName?: string;
-  // Data will be any not to cause a breaking change in the type declaration
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-}): { data: any; errors?: GraphQLFormattedError[] } => {
+}) {
   const { renderContext } = useServerContext();
   return JSON.parse(
     server.gql.executeQuerySync({
-      query,
+      query: typeof query === "string" ? query : print(query),
       variables,
       operationName,
       renderContext,
     }),
   );
-};
+}
